1fc67bc82c036a83d8cf8bd1e96c297a4b641eba
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-10-23 23:08:33 +0100

    fixing issues with gsap in js

---
+++ app.js
@@ -0,0 +1,31 @@
+function locomotive() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollProxy('#main', {
+        scrollTop(value) {
+            return arguments.length
+            ? locoScroll.scrollTo(value, 0, 0)
+            : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClient() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight,
+            };
+        },
+        pinType: document.querySelector("#main").computedStyleMap.transform
+        ? "transform"
+        : "fixed",
+    });
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.refresh();
+}
+locomotive();
